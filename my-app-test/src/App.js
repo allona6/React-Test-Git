e9ee163ade8ba065f2e857a42db23f52883f8131
Allona <36263017+allona6@users.noreply.github.com>
2018-02-08 12:51:50 +0100

    Commit2

--- my-app-test/src/App.js
+++ my-app-test/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload. Alfredo Llona
+          Vamos Peru
         </p>
       </div>
     );

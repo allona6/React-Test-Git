4b111b8bcdc9bd0a94ed1e921701676e0a8d9c55
e-aola <e-aola@PCBCN1319.gft.com>
2018-03-06 09:40:07 +0100

    commit change app.js

--- my-app-test/src/App.js
+++ my-app-test/src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload. Alfredo Llona
           Vamos Peru
           Aca pull
+          VAMOOO
         </p>
       </div>
     );
